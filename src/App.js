71eb8bfdc41acd6f107779ebde6328fa4a1b3ddf
Mustafa Babaei <mostafa.bsh@gmail.com>
2023-02-08 00:21:36 +0330

    added a costom video-player

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Button } from "@mui/material";
 import "./App.css";
 import Header from "./components/ui/Header";
 import SwiperSlider from "./components/ui/SwiperSlider";
+import VideoPlayer from "./components/videoPlayer/VideoPlayer";
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <SwiperSlider />
+      {/* <SwiperSlider /> */}
+      <VideoPlayer />
     </div>
   );
 }
